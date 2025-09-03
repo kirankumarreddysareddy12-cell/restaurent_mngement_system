3d6fe28223a0d43b9bd6f2ff1b691a736ecb1636
kirankumarreddysareddy12-cell <kirankumarreddysareddy12@gmail.com>
2025-09-03 07:32:28 +0530

    Add files via upload

---
+++ restarent mang.py
@@ -0,0 +1,11 @@
+#define menu of restaurent
+menu={
+   'pizza':99,
+   'pasta':120,
+   'burger':99,
+   'salad':70,
+   'coffee':80
+
+}
+print('welcome to kiran restaurent')
+print("pizza:rs99\npasta:rs120\nburger:rs99\nsalad:rs70\ncoffee:rs80")
